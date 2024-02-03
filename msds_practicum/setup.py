6b1959ab2406335823fa4ef171e384d343f19d13
Andrey Bilokonsky <abilokonsky@regis.edu>
2024-02-03 15:39:36 -0700

    initialized cookiecutter for data science into repository

---
+++ msds_practicum/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Angle detection using Computer Vision.',
+    author='Andrey Bilokonsky',
+    license='MIT',
+)
