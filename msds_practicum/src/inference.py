5d1aa585cdfc0e18fae7517df2a8bf8d624e5c1d
Andrey Bilokonsky <abilokonsky@regis.edu>
2024-02-21 00:40:49 -0700

    oops. gstreamer pipeline def got deleted before

--- msds_practicum/src/inference.py
+++ msds_practicum/src/inference.py
@@ -5,6 +5,32 @@ import tensorflow as tf
 # Load the saved TensorFlow model
 model = tf.keras.models.load_model('models/trained_models/Model_0002_CNN_ResNet__loss_sparse_categorical_crossentropy')
 
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 def preprocess_frame(frame):
     # Resize frame to match the input shape expected by the model (256x256)
     resized_frame = cv2.resize(frame, (256, 256))
